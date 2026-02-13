1c6dce3c5eb6fa877a8b390932f4faf468920251
tox <tox@tox.wtf>
2026-02-13 14:19:10 -0600

    !!feat: deoxidize

--- src/utils/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod cmd;
-pub mod float;
-pub mod log;
-pub mod str;
-pub mod ver;
--- src/utils/str.rs
+++
@@ -1,6 +0,0 @@
-/// Returns the basename of a string, ie everything after the final slash, or just the string if
-/// there are no slashes
-#[inline]
-pub fn basename(s: &str) -> &str {
-    s.rsplit_once('/').map_or(s, |s| s.1)
-}
